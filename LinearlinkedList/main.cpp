2af035cda5d0023f242db386778a88065e988ec1
Rao Bo <1441794016@qq.com>
2022-04-17 20:59:31 +0800

    LinearlinkedList

--- LinearlinkedList/main.cpp
+++ LinearlinkedList/main.cpp
@@ -51,9 +51,30 @@ Status ListIncert(LNode * L, int i, ElemType e){
     while(j < i - 1){
         p = p->next;
         j++;
+        if(p == NULL || i < 1)
+            return ERROR;
     }
     s->next = p->next;
     p->next = s;
+    return OK;
+}
+
+Status ListDelete(LNode * L, int i, ElemType & e){
+    //删除链表中第i个结点
+    if (i < 1)
+        return ERROR;
+    LNode * p = L;
+    int j = 0;
+    while(j < i - 1){
+        p = p->next;
+        j++;
+        if(p == NULL)
+            return ERROR;
+    }
+    LNode * q = p->next;
+    e = q->data;
+    p->next = p->next->next;
+    delete(q);
     return OK;
 }
 
@@ -66,6 +87,56 @@ Status ListTravel(LNode * L, Status(* travel)(ElemType e)){
     return OK;
 }
 
+Status MergeList(LNode * La, LNode * Lb, LNode * & Lc){
+    //两个链表的归并
+    //La Lb都是递增,归并成新的Lc
+    LNode * p = La;
+    LNode * q = Lb;
+    Lc = new LNode;
+    Lc->next = NULL;
+    LNode * b = Lc;
+    p = p->next;
+    q = q->next;
+    LNode * s;
+    while(p && q){
+        if(p->data <= q->data){
+            s = new LNode;
+            s->data = p->data;
+            b->next = s;
+            b = s;
+            p = p->next;
+        }
+        else{
+            s = new LNode;
+            s->data = q->data;
+            b->next = s;
+            b = s;
+            q = q->next;
+        }
+    }
+    if(p == NULL){
+        // 表示La已全部归并完
+        while(q){
+            s = new LNode;
+            s->data = q->data;
+            b->next = s;
+            b = s;
+            q = q->next;
+        }
+    }
+    else{
+        while(p){
+            s = new LNode;
+            s->data = p->data;
+            b->next = s;
+            b = s;
+            p = p->next;
+        }
+    }
+    return OK;
+
+}
+
 Status print(ElemType e){
     std::cout<<e<<std::endl;
 }
@@ -76,8 +147,18 @@ int main() {
     std::cin>>n;
     CreateList(List, n);
     ElemType e;
-    GetElem(List, 5, e);
-    std::cout<<e<<std::endl;
+//    GetElem(List, 5, e);
+//    std::cout<<e<<std::endl;
     ListIncert(List, 1, 0);
     ListTravel(List, print);
+    ListDelete(List, 5, e);
+    std::cout<<"e: "<<e<<std::endl;
+    ListTravel(List, print);
+
+    LNode * List2;
+    std::cin>>n;
+    CreateList(List2, n);
+    LNode * List3;
+    MergeList(List, List2, List3);
+    ListTravel(List3, print);
 }
