e5f5c9239c4dc59f2b6b0cb27e67ad1cf838f919
Rao Bo <1441794016@qq.com>
2022-04-18 17:08:21 +0800

    Link Queue

--- Queue/main.cpp
+++ Queue/main.cpp
@@ -2,6 +2,7 @@
 #define OK 1
 #define ERROR 0
 typedef double ElemType;
+typedef int Status;
 typedef struct QNode{
     ElemType  data;
     QNode * next;
@@ -11,6 +12,71 @@ typedef struct LinkQueue{
     QNode * front;
     QNode * rear;
 }LinkQueue;
+
+Status InitQueue(LinkQueue & L){
+    //创建一个空链队列
+    L.front = new QNode;
+    L.rear = L.front;
+    L.front->next = NULL;
+    return OK;
+}
+
+Status DestroyQueue(LinkQueue & L){
+    while(L.front){
+        L.rear = L.front->next;
+        delete(L.front);
+        L.front = L.rear;
+    }
+    return OK;
+}
+
+Status QueueIncert(LinkQueue & L, ElemType e){
+    // 将e插入链队列L
+    QNode * q = new QNode ;
+    q->data = e;
+    q->next = NULL;
+    L.rear->next = q;
+    L.rear = q;
+    return OK;
+}
+
+Status QueueDelete(LinkQueue & L, ElemType & e){
+    if(L.front == L.rear)
+        return ERROR;
+    e = L.front->next->data;
+    QNode * p = L.front->next;
+    if( L.front->next == L.rear){
+        L.rear = L.front;
+    }
+    L.front->next = L.front->next->next;
+    delete(p);
+    return OK;
+}
+
+Status QueueTravel(LinkQueue L, Status(* travel)(ElemType e)){
+    QNode * q = L.front->next;
+    while(q){
+        travel(q->data);
+        q = q->next;
+    }
+    return OK;
+}
+
+Status print(ElemType e){
+    std::cout<<e<<std::endl;
+    return OK;
+}
+
 int main() {
-   
+    LinkQueue L;
+    InitQueue(L);
+    QueueIncert(L, 1);
+    QueueIncert(L, 2);
+    QueueIncert(L, 3);
+    QueueIncert(L, 4);
+    QueueTravel(L,print);
+    ElemType  e;
+    QueueDelete(L, e);
+    std::cout<<e<<std::endl;
+    QueueTravel(L,print);
 }
